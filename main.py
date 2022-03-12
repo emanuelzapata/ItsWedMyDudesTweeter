5a5ed1fb44c4f83ef77d53a405309d17c1cbf60f
Emanuel Zapata <emanuelzapata@ymail.com>
2022-03-12 15:57:06 -0600

    committing removing comments

--- main.py
+++ main.py
@@ -4,17 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-# token = os.environ.get("API_KEY")
-# print(token)
-# auth = tweepy.OAuthHandler(os.environ.get("API_KEY"), os.environ.get("API_SECRET"))
-# auth.set_access_token(os.environ.get("ACCESS_TOKEN"), os.environ.get("ACCESS_SECRET"))
-
-# api = tweepy.API(auth)
-
-# image = api.media_upload('./assets/itswednesday.mp4')
-
-# api.update_status(status="Its Wednesday My Dudes!", media_ids=[image.media_id_string])
-
 def send_tweet(api):
     image = api.media_upload('./assets/itswednesday.mp4')
     api.update_status(status="Its Wednesday My Dudes!", media_ids=[image.media_id_string])
@@ -28,6 +17,6 @@ def authenticate(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET):
 def main():
     api = authenticate(os.environ.get("API_KEY"), os.environ.get("API_SECRET"), os.environ.get("ACCESS_TOKEN"), os.environ.get("ACCESS_SECRET"))
     send_tweet(api)
-    
+
 if __name__ == "__main__":
     main()
