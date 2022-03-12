b7a10ea1e71dc69a40ef2286a0b546937b471eaf
Emanuel Zapata <emanuelzapata@ymail.com>
2022-03-12 15:50:23 -0600

    committing main.py

--- main.py
+++ main.py
@@ -6,11 +6,28 @@ import os
 
 # token = os.environ.get("API_KEY")
 # print(token)
-auth = tweepy.OAuthHandler(os.environ.get("API_KEY"), os.environ.get("API_SECRET"))
-auth.set_access_token(os.environ.get("ACCESS_TOKEN"), os.environ.get("ACCESS_SECRET"))
+# auth = tweepy.OAuthHandler(os.environ.get("API_KEY"), os.environ.get("API_SECRET"))
+# auth.set_access_token(os.environ.get("ACCESS_TOKEN"), os.environ.get("ACCESS_SECRET"))
 
-api = tweepy.API(auth)
+# api = tweepy.API(auth)
 
-image = api.media_upload('./assets/itswednesday.mp4')
+# image = api.media_upload('./assets/itswednesday.mp4')
 
-api.update_status(status="Its Wednesday My Dudes!", media_ids=[image.media_id_string])
+# api.update_status(status="Its Wednesday My Dudes!", media_ids=[image.media_id_string])
+
+def send_tweet(api):
+    image = api.media_upload('./assets/itswednesday.mp4')
+    api.update_status(status="Its Wednesday My Dudes!", media_ids=[image.media_id_string])
+
+def authenticate(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET):
+    auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    return api
+
+def main():
+    api = authenticate(os.environ.get("API_KEY"), os.environ.get("API_SECRET"), os.environ.get("ACCESS_TOKEN"), os.environ.get("ACCESS_SECRET"))
+    send_tweet(api)
+    
+if __name__ == "__main__":
+    main()
